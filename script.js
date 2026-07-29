371fd474ad605d5f9eed15c53f7ed4824b5d8432
helloaurb-stack <helloaurb@gmail.com>
2026-07-29 13:26:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.querySelectorAll(".card button").forEach(button => {
-  button.addEventListener("click", function () {
-    alert("Product added! WhatsApp se order karein.");
-    window.open("https://wa.me/919315344841", "_blank");
-  });
-});
+
