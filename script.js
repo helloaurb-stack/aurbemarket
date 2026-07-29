3399ab1b02bfdb44c33748e27f3f6cb05e4ceeca
helloaurb-stack <helloaurb@gmail.com>
2026-07-29 14:05:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+console.log("AURB E-MARKET Website Loaded");
 
+
+function orderProduct(productName){
+
+let phone="919315344841";
+
+let message="Hello AURB E-MARKET, I want to order: "+productName;
+
+let url="https://wa.me/"+phone+"?text="+encodeURIComponent(message);
+
+window.open(url,"_blank");
+
+}
